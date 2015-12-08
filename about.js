59d9fc9c68497dafb6605c294373933bd8fde72c
dennis <ibanzajoe@gmail.com>
2015-12-08 13:24:59 -0800

    added session function to test login status

--- about.js
+++ about.js
@@ -10,7 +10,9 @@ var mongoose = require('mongoose'),
     request = require('request'),
     cheerio = require('cheerio'),
     EventEmitter = require('events').EventEmitter,
-    control = new EventEmitter();
+    control = new EventEmitter(),
+    secret = require('./config/secret')
+    session = require('express-session');
 
 var db = mongoose.connection;
 db.on('error', console.error);
@@ -20,6 +22,18 @@ var app = express();
 app.set('view engine', 'jade');
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static('public'));
+app.use(session({
+    secret: 'keyboard cat',
+    cookie: {}
+}));
+function testSession(req, res, next){
+    if(req.session.login==true){
+    next();
+    } else {
+        res.redirect('/');
+    }
+}
+
 var server = app.listen(3000, function(){
     var host = server.address().address;
     var port = server.address().port;
@@ -27,10 +41,37 @@ var server = app.listen(3000, function(){
 });
 mongoose.connect('mongodb://localhost/test');
 
-app.get('/addInfo', function(req, res){
-    res.render('addInfo');
+app.get('/', function(req, res){
+    res.render('login');
+    console.log(secret);
 });
-app.post('/insertAgent', function(req, res){
+app.post('/login', function(req, res){
+    console.log(req.body.username);
+    console.log(req.body.password);
+    console.log(secret.user.username);
+    console.log(secret.user.password);
+    if((secret.user.username == req.body.username) && (secret.user.password == req.body.password)){
+        req.session.login = true;
+        console.log(req.session.login);
+        res.render('loginScreen', {
+            username: req.body.username,
+            password: req.body.password
+        })
+
+    } else{
+        res.send('your username and/or password is incorrect')
+    }
+});
+
+app.get('/addInfo', testSession, function(req, res) {
+    if(req.session.login==true){
+        res.render('addInfo');
+    } else {
+        console.log('it did not work, fix it');
+    }
+
+    });
+app.post('/insertAgent', testSession, function(req, res){
     var info = new bout({
         pURL: req.body.pURL,
         name: req.body.name,
@@ -51,10 +92,10 @@ app.post('/insertAgent', function(req, res){
     })
 });
 
-app.get('/addList', function(req, res){
+app.get('/addList', testSession, function(req, res){
     res.render('listing');
 });
-app.post('/insert', function(req, res){
+app.post('/insert', testSession, function(req, res){
     var nList = new Lists({
         url: req.body.url,
         title: req.body.title,
@@ -67,10 +108,10 @@ app.post('/insert', function(req, res){
     })
 });
 
-app.get('/addService', function (req, res) {
+app.get('/addService', testSession, function (req, res) {
     res.render('service');
 });
-app.post('/insertService', function (req, res) {
+app.post('/insertService', testSession, function (req, res) {
     var serv = new service({
         url: req.body.url,
         descript: req.body.descript
@@ -81,10 +122,10 @@ app.post('/insertService', function (req, res) {
     })
 });
 
-app.get('/addTest', function (req, res) {
+app.get('/addTest', testSession, function (req, res) {
     res.render('testimony');
 });
-app.post('/insertTesti', function (req, res) {
+app.post('/insertTesti', testSession, function (req, res) {
     var testz = new test({
         url: req.body.url,
         testimony: req.body.testimony,
@@ -96,7 +137,9 @@ app.post('/insertTesti', function (req, res) {
     })
 });
 
-app.get('/seeList', function(req, res) {
+app.get('/seeList', testSession, function(req, res) {
+//    req.session.tumadre = req.session.tumadre? req.session.tumadre+1:1;
+//    console.log(req.session.tumadre);
     bout.find({name: 'Anna Lee'}, function (err, x) {
         Lists.find(function (err, y) {
             service.find(function (err, z) {
@@ -112,12 +155,28 @@ app.get('/seeList', function(req, res) {
         });
     });
 });
+app.get('/logOut', testSession, function(req, res){
+    req.session.login=false;
+    res.send('you are logged out');
+    console.log(req.session.login);
+})
 
 
-app.get('/addContact', function (req, res) {
+app.get('/findAgent', testSession, function(req,res){
+    res.render('findAgent');
+});
+app.post('/updateAgent', testSession, function(req,res){
+    console.log(req.body.name);
+    bout.findOne({name:req.body.name}, function(err, name){
+    console.log(bout);
+    })
+})
+
+
+app.get('/addContact', testSession, function (req, res) {
     res.render('contact');
 });
-app.post('/insertContact', function (req, res) {
+app.post('/insertContact', testSession, function (req, res) {
     var cont = new contact({
         facebook: req.body.facebook,
         twitter: req.body.twitter,
@@ -127,7 +186,7 @@ app.post('/insertContact', function (req, res) {
         res.send(cont);
     });
 });
-app.get('/createAgent', function(req, res) {
+app.get('/createAgent', testSession, function(req, res) {
     var nbout = new bout({
         pURL: 'xxxx',
         name: 'Anna Lee',
@@ -145,7 +204,7 @@ app.get('/createAgent', function(req, res) {
         res.redirect('/agentdb');
     });
 });
-app.get('/agentdb', function (req, res) {
+app.get('/agentdb', testSession, function (req, res) {
     bout.findOne({name: 'Anna Lee'}, function (err, bout) {
         if (err) return console.error(err);
         console.log(bout.name);
@@ -160,7 +219,7 @@ app.get('/agentdb', function (req, res) {
     });
 
 });
-app.get('/database', function(req, res){
+app.get('/database', testSession, function(req, res){
     Lists.find(function(err, lists) {
         res.send(lists);
     })
